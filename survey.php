b3b36bab5e93dfbacaa8c91ef1bae5eb73d24175
jgs3817 <Josh@Joshuas-MacBook-Pro.local>
2020-06-11 16:08:53 +0700

    Minor fix

--- survey.php
+++ survey.php
@@ -35,22 +35,18 @@
     if ($consent == 0){
         $id = 1;
     }
+    else{
+        //Insert to other table
+        $query = "INSERT INTO pollresult (id) VALUES ($id)";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+    }
 
     $_SESSION['id'] = $id;
     $_SESSION['oiflag'] = 0;
     $_SESSION['tpflag'] = 0;
     $_SESSION['mmflag'] = 0;
     $_SESSION['imflag'] = 0;
-
-    //Insert to other table
-    /*$db = pg_connect("host=localhost port=5432 dbname=neusense user=neusenseuser password=password");
-    $query = "INSERT INTO pollresult (id) VALUES ($id)";
-    $result = pg_query($query);*/
-    $query = "INSERT INTO pollresult (id) VALUES ($id)";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute();
-
-    echo "End of survey.php";
     
     //Go to overview page
     header('Location: overview_page.html');
